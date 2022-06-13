6a2fd4516b1ebbdd649f97966d8011e81349ac85
Amaben2020 <uzochukwubenamara@gmail.com>
2022-06-13 16:07:47 +0100

    using bcrypt to hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,36 @@
 import User from "../models/User.js";
-
+import bcrypt from "bcrypt";
 export const register = async (req, res, next) => {
-  const { username, email, password } = req.body;
-  const user = new User({
-    username,
-    email,
-    password,
-  });
   try {
+    const { username, email } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    console.log(hash);
+    const user = new User({
+      username,
+      email,
+      password: hash,
+    });
+
+    const newUser = await user.save();
+    res.status(200).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { username, email } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    console.log(hash);
+    const user = new User({
+      username,
+      email,
+      password: hash,
+    });
+
     const newUser = await user.save();
     res.status(200).json(newUser);
   } catch (error) {
